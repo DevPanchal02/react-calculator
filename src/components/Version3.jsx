f7a3dafce01c2f214d57b65ef1bacc77673d95af
Dev Panchal <90882951+DevPanchal02@users.noreply.github.com>
2023-04-01 21:30:54 -0400

    Added function handling

--- src/components/Version3.jsx
+++ src/components/Version3.jsx
@@ -2,7 +2,18 @@ import { useState } from 'react'
 import Styles from './Styles.css'
 
 function App() {
-  const [result, setResult] = useState(0);
+  const [result, setResult] = useState("");
+
+  function handleClick (e) {
+    setResult(result.concat(e.target.name));
+    console.log(e.target.name);
+  }
+  function handleClear(){
+    setResult("");
+  }
+  function handleBackSpace() {
+
+  }
 
   return (
     <div>
@@ -13,24 +24,24 @@ function App() {
     </div>
 
     <div className='keypad'>
-      <button>Clear</button>
-      <button>C</button>
-      <button>&divide;</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>&times;</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>&ndash;</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>+</button>
-      <button>0</button>
-      <button>.</button>
-      <button>=</button>
+      <button onClick={handleClear} id='clear'>Clear</button>
+      <button onClick={handleBackSpace} id='backspace'>C</button>
+      <button name = '/'onClick={handleClick}>&divide;</button>
+      <button name = '7'onClick={handleClick}>7</button>
+      <button name = '8'onClick={handleClick}>8</button>
+      <button name = '9'onClick={handleClick}>9</button>
+      <button name = '*'onClick={handleClick}>&times;</button>
+      <button name = '4'onClick={handleClick}>4</button>
+      <button name = '5'onClick={handleClick}>5</button>
+      <button name = '6'onClick={handleClick}>6</button>
+      <button name = '-'onClick={handleClick}>&ndash;</button>
+      <button name = '1'onClick={handleClick}>1</button>
+      <button name = '2'onClick={handleClick}>2</button>
+      <button name = '3'onClick={handleClick}>3</button>
+      <button name = '+'onClick={handleClick}>+</button>
+      <button name = '0'onClick={handleClick}>0</button>
+      <button name = '.'onClick={handleClick}>.</button>
+      <button onClick={handleClick}>=</button>
     </div>
 
     </div>
